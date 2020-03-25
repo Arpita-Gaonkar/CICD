8ecda71c74f30f59e81db6175e20c7704d8ea343
Arpita <gaonkar.a@husky.neu.edu>
2020-03-25 02:51:59 -0400

    cicd commit

---
+++ src/main/java/com/example/demo/GitJenkins1Application.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitJenkins1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitJenkins1Application.class, args);
+		
+		System.out.println("1st jenkins project");
+		System.out.println("2st jenkins project");
+		System.out.println("4st jenkins project");
+		System.out.println("CICD");
+		System.out.println("2nd CICD");
+
+
+
+	}
+
+}
